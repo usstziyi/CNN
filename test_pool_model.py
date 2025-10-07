e101df7d0de6e9f1da9a960decbe6b7951e6ca9c
007 <007lookworld@gmail.com>
2025-10-07 15:18:58 +0800

    test(pool_model): 添加多通道输入测试用例
    添加对多通道输入的测试用例，验证池化层在不同输入维度下的行为

--- test_pool_model.py
+++ test_pool_model.py
@@ -4,6 +4,7 @@ from pool import Pool2D_Model
 
 
 def main():
+    # 测试最大池化层
     model = Pool2D_Model(
         mode='max',
         kernel_size=2,
@@ -14,6 +15,7 @@ def main():
     print(x)
     print(model(x))
 
+    # 测试平均池化层
     model = Pool2D_Model(
         mode='avg',
         kernel_size=2,
@@ -22,6 +24,19 @@ def main():
     )
     print(x)
     print(model(x))
+
+
+    # 测试多通道输入
+    x = torch.arange(32, dtype=torch.float32).reshape((1, 2, 4, 4))
+    model = Pool2D_Model(
+        mode='max',
+        kernel_size=2,
+        stride=2,
+        padding=0
+    )
+    print(x.shape)
+    # 池化层不合并通道，卷积层合并通道
+    print(model(x).shape)
 
 if __name__ == '__main__':
     main()
